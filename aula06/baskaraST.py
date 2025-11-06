318b31295b4c5ef5371ef8a368f284030f211b59
kaiomartinez <kaiomartinez831@gmail.com>
2025-11-05 21:47:09 -0300

    mudando a formula de baskara.st

--- aula06/baskaraST.py
+++ aula06/baskaraST.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import math as mt
 
+def calculo(delta):
+    valor = (mt.sqrt(delta)) / (2 * a)
+    return valor
+
+
 st.header("Calculadora de Bhaskara")
 st.write("Calculadora de raízes de uma \n de" \
 "uma equação do segundo grau")
@@ -15,16 +20,19 @@ if st.button("Calcular raízes"):
         a = float(a)
         b = float(b)
         c = float(c)
-        delta = b**2 - 4 * a * c
+        delta = pow(b, 2) - 4 * a * c
         st.write(f"O valor de Δ é: {delta}")
         if delta < 0:
             st.warning("A equação não possui raízes reais.")
         elif delta == 0:
-            raiz = -b / (2 * a)
+            raiz = calculo(delta)
+            st.write(f"A equação possui uma raiz real: x = {raiz}")
         else:
-            raiz1 = (-b + mt.sqrt(delta)) / (2 * a)
-            raiz2 = (-b - mt.sqrt(delta)) / (2 * a)
+            raiz1 = - (b + calculo(delta))
+            raiz2 = - (b - calculo(delta))
             st.write(f"A equação possui duas raízes reais: x1 =  {raiz1} e x2 =  {raiz2}")
 
     except ValueError:
         st.error("Por favor, insira valores válidos.")
+    except ZeroDivisionError:
+        st.error("O valor de 'a' não pode ser zero.")
